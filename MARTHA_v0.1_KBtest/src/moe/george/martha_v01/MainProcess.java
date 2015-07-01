d1910e1fb14371f9cc3f60b984c1d2a07f46a507
George Moe <geomo3@gmail.com>
2015-07-01 10:24:48 -0500

    MARTHA is now versioned! Introducing MARTHA v0.1.

---
+++ MARTHA_v0.1_KBtest/src/moe/george/martha_v01/MainProcess.java
@@ -0,0 +1,158 @@
+/* ===================================================
+ * MARTHA - Theory of Mind Attempt 1
+ * Mental-state Aware Real-time THinking Assistant
+ * ------------------------------------------------
+ * This fork attempts to implement a theory of mind
+ * on top of continuous consciousness.
+ * ------------------------------------------------
+ * NOTE: Comments added while working.
+ * ===================================================*/
+package moe.george.martha_v01;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.cyc.base.CycConnectionException;
+import com.cyc.kb.client.ContextImpl;
+import com.cyc.kb.exception.KBApiException;
+import com.cyc.session.SessionCommunicationException;
+import com.cyc.session.SessionConfigurationException;
+import com.cyc.session.SessionInitializationException;
+
+public class MainProcess {
+
+	// ============ Declare constants used throughout the program ============
+
+	/*
+	 * The path at which the init folder containing MARTHA knowledge can be
+	 * found.
+	 */
+	public final static String init_folder = "birthday";
+	public final static String history_file = "history.martha";
+
+	// The context in which all assertions are made.
+	public static String context = "RealTimeMt";
+
+	/*
+	 * Create a new context each time so that everything asserted before is
+	 * flushed.
+	 */
+	private static Boolean new_context_each_time = true;
+
+	// ============ Declare variables used throughout ============
+	// The MARTHA Engine, through which all operations are performed.
+	public static Martha martha;
+
+	// Main Process
+	public static void main(String[] args)
+			throws SessionConfigurationException,
+			SessionCommunicationException, SessionInitializationException,
+			CycConnectionException, KBApiException {
+
+		/*
+		 * Generate a new context name based on the exact time, down to the
+		 * second. The purpose of this is so that I don't have to keep
+		 * restarting Cyc whenever I make database changes, which is very slow.
+		 */
+		if (new_context_each_time) {
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss");
+			Date dt = new Date();
+
+			// Formats to a string of numbers based on the date and time.
+			String timestamp = sdf.format(dt);
+
+			context = timestamp + "Mt";
+			System.out.println(context);
+		}
+
+		// Let the user know that we're starting up
+		System.out.println("Initializing MARTHA...");
+		martha = new Martha(context); // Create a new instance of the MARTHA
+										// engine, specifying the assert
+										// context.
+
+		// Create MARTHA subcontext
+		String subcontext = ContextImpl.findOrCreate("MARTHA_" + context)
+				.toString();
+		martha.interpret(">(genlMt " + subcontext + " " + context + ")");
+
+		// Initialize the Cyc database from the contents of the init folder by
+		// feeding them line by line through the MARTHA interpreter.
+		Pattern p = Pattern.compile("\\.martha$");
+		for (File f : (new File(init_folder)).listFiles()) {
+			// Make sure files have the ".martha" extension EXACTLY AT THE END
+			Matcher m = p.matcher(f.toString());
+
+			// If they do, load them.
+			if (m.find()) {
+				System.out.print("Loading " + f.toString() + "... ");
+				try {
+					// Each file is loaded through the Martha Engine by name.
+					martha.initFromFile(f.toString());
+					System.out.println("Done.");
+				} catch (IOException e) {
+					/*
+					 * Catch problems initializing from the file; no big deal,
+					 * don't need to crash the program.
+					 */
+					e.printStackTrace();
+					System.out.println("Error initializing from file " + f
+							+ ".");
+				}
+			}
+		}
+
+		// Move MARTHA to the MARTHA_ subcontext.
+		martha.changeContext(subcontext);
+
+		// Update the internal Java list of actions available to Martha.
+		martha.updateActionSet();
+
+		// Print friendly cool MARTHA banner
+		System.out
+				.println("\n\n===============================================");
+		System.out.println("Welcome to MARTHA");
+		System.out.println("Mental-state Aware Real-time THinking Assistant");
+		System.out.println("Copyright (c) George Moe 2015");
+		System.out
+				.println("===============================================\n\n");
+
+		martha.start();
+
+		// Prompt loop; this is what presents and handles the "MARTHA: " prompt.
+		// Rather than having to evaluate and execute commands here,
+		// Input here is asserted into the KB to be evaluated on the
+		// next cycle of the consciousness.
+		while (true) {
+			String input = getInput("MARTHA: "); // Get input from user with the
+													// "MARTHA: " prompt.
+			martha.interpretFromUser(input); // Interpret user input
+		}
+
+	}
+
+	// Since console input from the traditional System.console.readline()
+	// doesn't work in Eclipse,
+	// I'm using this workaround instead.
+	// For details on operation, look at the earlier MARTHA MainProcess.java
+	// code.
+	private static String getInput(String prompt) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(prompt);
+		String input = "";
+		try {
+			input = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return input;
+	}
+}
---
+++ MARTHA_v0.1_KBtest/src/moe/george/martha_v01/MarthaConsciousness.java
@@ -0,0 +1,117 @@
+/*===========================================
+ * MARTHA Consciousness
+ * ------------------------------------------
+ * A endlessly looping thread that continuously
+ * calls for evaluation and execution methods
+ * in the MARTHA engine.
+ *===========================================*/
+
+package moe.george.martha_v01;
+
+public class MarthaConsciousness implements Runnable {
+
+	// Instantiate some variables
+
+	private String name = "m_conscious";
+
+	/*
+	 * With this version of MARTHA, it is possible for MARTHA to enter different
+	 * states of consciousness. State 0, or dreaming, is intended to be MARTHA's
+	 * idle state in which it can freely think up LONG chains of actions, with
+	 * possible utility in the future. State 1 is like dreaming, but with
+	 * shorter chains with seeds based in what was recently queried or said.
+	 * State 2 is a goal driven search that is just a complete backwards-chain,
+	 * intended to be used to find actions that can be used to fulfill a goal.
+	 * 
+	 * Martha can change freely between these states, depending on recent idle
+	 * cycles. There are mechanisms here to control this.
+	 * 
+	 * This is slated to be implemented in a future Martha Consciousness.
+	 */
+	// private int state = 2;
+	/*
+	 * 0 = dream 1 = contextual search 2 = goal driven search
+	 */
+
+	// Martha Conscioussness's reference to the main MARTHA Engine.
+	private Martha martha;
+
+	// The Martha Consciousness thread.
+	private Thread t;
+
+	// Get the Martha instance from the MainProcess.
+	public MarthaConsciousness(Martha m) {
+		martha = m;
+	}
+
+	// The consciousness loop.
+	@Override
+	public void run() {
+		int counter = 0; // To keep track of cycles
+		while (true) {
+			counter++; // increment counter
+
+			/*
+			 * NOTE: Much of the things relating to state have been commented
+			 * out from this implementation of Martha Conciousness, because it's
+			 * not the focus here. To see the original text, check out
+			 * TheoryOfMind1, or go back in a previous git version.
+			 */
+
+			// Handle direct goals first...
+			martha.planForGoals();
+
+			// ...then take a look at less obvious intentions.
+			martha.explore();
+
+			/*
+			 * If we've accumulated 10 cycles (DEBUG: just 1 cycle for now)
+			 * worth of plans, Evaluate them and execute the results.
+			 */
+			if (counter % 1 == 0) {
+
+				//Execute the queued plans.
+				martha.execute();
+
+				// DEBUG: Dump of USER/MARTHA knowledge.
+				System.out.println("USER knows:");
+				martha.interpretFromUser("?(knows USER ?WHAT)");
+				System.out.println("USER believes:");
+				martha.interpretFromUser("?(beliefs USER ?WHAT)");
+				System.out.println("MARTHA knows:");
+				martha.interpretFromUser("?(knows MARTHA ?WHAT)");
+				
+				//DEBUG: Pause to allow the operator to inspect debug code. 
+				//Print dots to show time.
+				for (int i = 0; i < 5; i++) {
+					System.out.print(".");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				System.out.println();
+			}
+
+			// Shift focus to next cycle
+			Martha.incrementFocusTicker();
+		}
+	}
+
+	// Spawn this new thread.
+	public void start() {
+		// Let the user know it's all starting up.
+		System.out.println("Starting Martha Consciousness... " + name);
+		if (t == null) {
+			t = new Thread(this, name);
+			t.start();
+		}
+	}
+
+	public void setState(int s) {
+		// state = s; //UNUSED for now.
+	}
+
+}
